0c7e9fd4c3b58448099340609df954d5a97ac950
Alexander Laurie <alexander.l+1@speedandfunction.com>
2021-01-21 16:47:31 -0500

    Cors added

---
+++ handlers/cors.go
@@ -0,0 +1,8 @@
+package handlers
+
+import "net/http"
+
+func setupResponse(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+}
